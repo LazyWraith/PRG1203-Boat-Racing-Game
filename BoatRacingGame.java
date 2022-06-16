bc6cdcdcc286dc0060e61ebf2937efff4865ca6a
AlphaTheWoof <97719857+AlphaTheWoof@users.noreply.github.com>
2022-06-16 09:07:24 +0800

    Create files

---
+++ BoatRacingGame.java
@@ -0,0 +1,3 @@
+/* This is the main program of the game.
+ * 
+*/
---
+++ Game.java
@@ -0,0 +1,7 @@
+/* This is the main engine of the game. All game code resides here and can be called from the main program.
+ * 
+*/
+
+public class Game() {
+    
+}
